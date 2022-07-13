25077d371f69d4499eca857970f30c8d3b58bb24
melchuajl <102275204+melchuajl@users.noreply.github.com>
2022-07-13 20:07:20 +0800

    fixing rebase error

--- models/index.js
+++ models/index.js
@@ -1,11 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize({
-//     database: 'dcimlucjej1p1k', 
-//     username: 'asfaamshpartmm', 
-//     password: 'f499ccb40586ea8dd3b08b81019ef57108bcce614b88a1a131c0e7a1aaad35b0',
+// const sequelize = new Sequelize('dcimlucjej1p1k', 'asfaamshpartmm', 'f499ccb40586ea8dd3b08b81019ef57108bcce614b88a1a131c0e7a1aaad35b0', {
 //     host: 'ec2-3-217-14-181.compute-1.amazonaws.com',
-//     port: 5432,
 //     dialect: 'postgres',
 //     dialectOptions: {
 //         ssl: {
@@ -15,10 +11,10 @@ const { Sequelize } = require('sequelize');
 //     }
 // });
 
-const sequelize = new Sequelize("lesson_db", "postgres", "sandwich94", {
-    host: "localhost",
-    dialect: "postgres",
-  });
+const sequelize = new Sequelize('lesson_db', 'melissachua', '', {
+    host: 'localhost',
+    dialect: 'postgres'
+})
 
 // IMPORT MODELS
 const Product = require('./product.model')(sequelize);
