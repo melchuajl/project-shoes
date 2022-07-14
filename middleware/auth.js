960716e1645678ea9d8c71c4b9cd06c7f1e0246e
melchuajl <102275204+melchuajl@users.noreply.github.com>
2022-07-14 10:09:30 +0800

    fixed authentication middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,34 +1,35 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const privateKey = process.env.JWT_SECRET_KEY; 
+const privateKey = process.env.TOKEN_KEY;
 
 module.exports = {
 
     isLoggedIn: async (req, res, next) => {
 
-        const token = req.headers.token;
+        const authHeader = req.headers['authorization'];
+        const token = authHeader.split(' ')[1];
 
         if (!token) {
-            res.status(401); // status code for 'Unauthorised: authentication required'
-            return res.json({ message: "Please log in"});
+            res.status(401); // status code for 'Authentication required'
+            return res.json({ message: "Please log in" });
         }
 
         jwt.verify(token, privateKey, (error, user) => {
             if (error) {
-                res.status(401).send({ message: "Session has expired"}); 
-                console.log(error)
+                res.status(401);
+                return res.json({ message: "Invalid token, please log in again" });
             }
-            req.user = user; 
-            next(); 
+            req.user = user;
+            return next();
         })
     },
 
     isOwner: async (req, res, next) => {
 
-        if(req.user.role.toLowerCase() !== 'owner') {
+        if (req.user.role.toLowerCase() !== 'owner') {
             res.status(403); // status code for 'Forbidden'
-            return res.json({ message: "Owner access only"})
+            return res.json({ message: "Owner access only" })
         }
-        next(); 
+        next();
     }
 }
